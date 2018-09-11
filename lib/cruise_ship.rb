5d27aa0c99025db0dc7bb3ecbe4d7ed953816a39
Mikkel Ridley <mikkel250@gmail.com>
2018-09-11 04:52:31 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,10 +1,10 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+passengers = {
+ suite_a: "Amanda Presley", 
+ suite_b: "Seymour Hoffman", 
+ suite_c: "Alfred Tennyson", 
+ suite_d: "Charlie Chaplin", 
+ suite_e: "Crumpet the Elf"
+ }
 winner = ""
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
